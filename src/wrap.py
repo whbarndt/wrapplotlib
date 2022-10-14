cdbc7e37db8b63cc03640e728afe23d6726926ed
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2022-10-13 16:35:49 -0800

    feat: template bar chart example

---
+++ src/wrap.py
@@ -0,0 +1,25 @@
+# Bar Chart (Wrapper Example)
+
+import matplotlib.pyplot as plt
+
+args = {
+    'title': 'Bar Chart',
+    'xlabel': 'X Axis',
+    'ylabel': 'Y Axis',
+    'color': 'blue',
+    'height': 5,
+    'width': 10,
+}
+
+def bar_chart(x, y, args):
+    plt.figure(figsize=(args['width'], args['height']))
+    plt.bar(x, y, color=args['color'])
+    plt.title(args['title'])
+    plt.xlabel(args['xlabel'])
+    plt.ylabel(args['ylabel'])
+    plt.show()
+ 
+plt.xlabel("Courses offered")
+plt.ylabel("No. of students enrolled")
+plt.title("Students enrolled in different courses")
+plt.show()
