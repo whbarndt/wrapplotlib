94f175aaac8407faccd3eb342fb53aa118473125
Hunter Barndt <whbarndt@protonmail.com>
2022-10-13 18:05:29 -0800

    Revert "Small edits to example.py,"
    This reverts commit dcda8e58577e5118cf9b3edd362ab4ffee8097ae.

--- example.py
+++ example.py
@@ -4,47 +4,40 @@ import datetime
 #import pandas
 
 # label='t = '+str(t[i])
-def plot(x, y, axis_labels=['x','y'], legend=False, tick_label_size='small'):
-    # Defaults
-    default_axis_labels = ['x','y']
-    default_tick_font_size = 10
-    default_figure_size = (12,8)
-
+def plot(x, y, axis_labels=['x','y'], legend=False, tick_label_size='large'):
     # Figure and Plot Definition
-    fig, ax = plt.subplots(figsize=default_figure_size)
-
+    fig, ax = plt.subplots(figsize=(10,8))
+    plt.plot(x, y)
     # Setting Axis Labels
+    # Pyplot
     #plt.xlabel(axis_labels[0]) 
     #plt.ylabel(axis_labels[1])
+    # OOP implementation
     ax.set_xlabel(axis_labels[0])  
     ax.set_ylabel(axis_labels[1])
-
     # Legend
     if legend is True:
         plt.legend()
-
     # Datetime Labels
     if datetime.datetime in x:
         ax.xaxis.set_major_formatter(mdates.DateFormatter(partdatenameformat))
-
     # Tick Sizes
     def set_tick_size(fontsize):
-        ax.tick_params(axis='x', labelsize=fontsize)
-        ax.tick_params(axis='y', labelsize=fontsize)
+        for tick in ax.xaxis.get_major_ticks():
+            tick.label.set_fontsize(fontsize)
+        for tick in ax.yaxis.get_major_ticks():
+            tick.label.set_fontsize(fontsize)
     if tick_label_size == 'large':
         set_tick_size(25)
     elif tick_label_size == 'medium':
         set_tick_size(17.5)
     elif tick_label_size == 'small':
-        set_tick_size(10)
-
-    # Plot and Save Figure
-    ax.plot(x, y)
+        set_tick_size(5)
+    # Save Figure
     #savefig('plot.png', transparent=True)
-    #plt.show()
+    plt.show()
     return
 
-'''
 # Wave Parameters:
 A = 1           # Amplitude
 k = 2           # Wavenumber 
@@ -60,4 +53,5 @@ labels=['x', 'f(x)']
 #for i in range(len(t)):
 y = A*np.cos(k*x-w*t)
 plot(x, y, axis_labels=labels)
-'''
+    
+
